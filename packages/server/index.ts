9238b278a5a3bb79fb8199391d2de53be29a31cf
armand <armand.salle@gmail.com>
2024-05-01 18:15:30 +0200

    make things work

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono"
+import { serveStatic } from "hono/bun"
 import { logger } from "hono/logger"
 
 const app = new Hono()
 app.use(logger())
 
-app.get("/", (c) => c.text("Hello Bun!"))
+app.get("/api", (c) => c.text("Hello Bun!"))
+
+app.get("*", serveStatic({ root: "../frontend/dist" }))
+app.get("*", serveStatic({ path: "../frontend/dist/index.html" }))
 
 Bun.serve({
   port: process.env.PORT || 3000,
