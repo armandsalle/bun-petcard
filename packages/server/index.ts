a67c81540a271c0362638b98811f77595f9774e1
armand <armand.salle@gmail.com>
2024-05-02 21:32:50 +0200

    fix 404 route

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,8 +7,20 @@ app.use(logger())
 
 const router = app.get("/api", (c) => c.text("Hello Bun!"))
 
-app.get("*", serveStatic({ root: "../frontend/dist" }))
-app.get("*", serveStatic({ path: "../frontend/dist/index.html" }))
+app.get(
+  "*",
+  serveStatic({
+    root: "../frontend/dist",
+    // /family /whatever => serve the index.html file
+    // /plop.js /logo.svg => serve the needed file
+    rewriteRequestPath: (path) => {
+      if (path.includes(".")) {
+        return path
+      }
+      return "/"
+    },
+  }),
+)
 
 Bun.serve({
   port: process.env.PORT || 3000,
