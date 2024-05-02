1635476867ca18dccb6cdca8ab215b2137da7c53
armand <armand.salle@gmail.com>
2024-05-03 00:21:14 +0200

    chore: Update API endpoint in frontend code

--- packages/frontend/src/lib/api.ts
+++ packages/frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
 import { type AppRouter } from "@server/index"
 import { hc } from "hono/client"
 
-export const api = hc<AppRouter>("http://localhost:5173/").api
+export const api = hc<AppRouter>("/").api
